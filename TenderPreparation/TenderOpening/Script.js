3d80f2711d5f606d5549ea80b1178119a44302cd
Amitp <cscvlemanora@gmail.com>
2023-03-16 02:27:41 +0530

    creating tender publish file and all other important files attached which is important

---
+++ TenderPreparation/TenderOpening/Script.js
@@ -0,0 +1,6 @@
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]') );
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {return new bootstrap.Tooltip(tooltipTriggerEl); });
+
+    $("#checkAll").click(function () {
+        $(".check").prop('checked', $(this).prop('checked'));
+    });
